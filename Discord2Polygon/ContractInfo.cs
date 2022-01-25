f716957fbd8f3323947b1b373b778fbccfe0859c
alexander <einfachalex@sags-per-mail.de>
2022-01-25 23:51:56 +0100

    create contractinfo struct to store information of our contract

---
+++ Discord2Polygon/ContractInfo.cs
@@ -0,0 +1,15 @@
+﻿namespace Discord2Polygon;
+
+public struct ContractInfo
+{
+    public string address { get; }
+    public string abi { get; }
+    public int decimals { get; }
+
+    public ContractInfo(string address, string abi, int decimals)
+    {
+        this.address = address;
+        this.abi = abi;
+        this.decimals = decimals;
+    }
+}
