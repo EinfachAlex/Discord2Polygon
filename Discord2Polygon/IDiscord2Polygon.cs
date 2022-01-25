e6e4355a32220784a199b24dd453829a9ad9b8f7
alexander <einfachalex@sags-per-mail.de>
2022-01-25 23:51:11 +0100

    add interface to define required methods for Discord2Polygon Classes

---
+++ Discord2Polygon/IDiscord2Polygon.cs
@@ -0,0 +1,10 @@
+﻿using Nethereum.RPC.Eth.DTOs;
+
+namespace Discord2Polygon;
+
+public interface IDiscord2Polygon
+{
+    public Task<TransactionReceipt> sendToPolygon(SendToPolygonDTO sendToPolygonDTO);
+    
+    public event EventHandler<ReceiveFromPolygonDTO> receiveFromPolygon;
+}
