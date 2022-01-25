133674c6055ff2facb0de2ca365dd1195175a4a4
alexander <einfachalex@sags-per-mail.de>
2022-01-25 23:50:54 +0100

    add DTO objects for communication with contract methods / events

---
+++ Discord2Polygon/ReceiveFromPolygonDTO.cs
@@ -0,0 +1,17 @@
+﻿using Nethereum.ABI.FunctionEncoding.Attributes;
+using Nethereum.Contracts;
+
+namespace Discord2Polygon;
+
+[Event("receiveFromPolygonEvent")]
+public class ReceiveFromPolygonDTO : FunctionMessage
+{
+    [Parameter("uint256", "discordID", 1, true)]
+    public ulong discordID {get; set;}
+
+    [Parameter("uint256", "blockNumber", 2, true)]
+    public ulong blockNumber {get; set;}
+
+    [Parameter("uint256", "amount", 3, false)]
+    public ulong amount {get; set;}
+}
---
+++ Discord2Polygon/SendToPolygonDTO.cs
@@ -0,0 +1,17 @@
+﻿using Nethereum.ABI.FunctionEncoding.Attributes;
+using Nethereum.Contracts;
+
+namespace Discord2Polygon;
+
+[Function("sendToPolygon", "bool")]
+public class SendToPolygonDTO : FunctionMessage
+{
+    [Parameter("uint256", "syncSumDiscordBalances", 1)]
+    public int syncSumDiscordBalances { get; set; }
+        
+    [Parameter("address", "addressOfDiscordUser", 2)]
+    public string addressOfDiscordUser { get; set; }
+        
+    [Parameter("uint256", "payoutAmount", 3)]
+    public int payoutAmount { get; set; }
+}
